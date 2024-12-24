06539f4fae50f1488f07a4098432fe0860d57090
Srijan Bajracharya <wierdo_97@live.com>
2024-12-24 16:58:43 +0545

    chore: fix lint

--- src/app/api/places/route.ts
+++ src/app/api/places/route.ts
@@ -2,7 +2,7 @@ import { apiHandler } from "@/helpers/api-handler";
 import { db } from "@/server/db";
 import { places } from "@/server/db/schema";
 
-export const GET = apiHandler(async (request: Request) => {
+export const GET = apiHandler(async () => {
   const contains = await db.select().from(places);
   return Response.json(contains);
 });
